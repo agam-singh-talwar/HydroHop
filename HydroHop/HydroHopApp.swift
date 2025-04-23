6d67caf831367f499e4144b67ceff888e3811cac
agam-singh-talwar <87700905+agam-singh-talwar@users.noreply.github.com>
2025-04-22 20:51:05 -0400

    Initial Commit

---
+++ HydroHop/HydroHopApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydroHopApp.swift
+//  HydroHop
+//
+//  Created by Agam Singh Talwar on 2025-04-22.
+//
+
+import SwiftUI
+
+@main
+struct HydroHopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
